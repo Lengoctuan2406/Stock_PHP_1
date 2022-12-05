ddd4a3e6f7aac62a4011797a31bbec6bfecf7897
Lê Ngọc Tuấn <lengoctuan2406@gmail.com>
2022-12-05 23:19:31 +0700

    Update

---
+++ Test/index.php
@@ -0,0 +1,57 @@
+<?php
+// $arr["John"] = 2;
+// $arr["Mary"] = 1;
+
+// $arrs[0] = $arr;
+
+// $arr["John"] = 2;
+// $arr["Mary"] = 1;
+
+// $arrs[1] = $arr;
+
+// $arr["John"] = 3;
+// $arr["Mary"] = 1;
+
+// $arrs[count(arrs)+1] = $arr;
+
+// $myJSON = json_encode($arrs);
+// echo $arrs[2]["John"]." ";
+// echo var_dump($myJSON);
+
+// $jsonitem = file_get_contents($_SERVER['DOCUMENT_ROOT']."/Stock_market_PHP_1/Data/account.json");
+// // echo var_dump($jsonitem);
+// $objitems = json_decode($jsonitem);
+// echo $objitems->username;
+
+//  $findById = function($id) use ($objname) {
+//   foreach (json_decode($objname) as $friend) {
+//     if ($friend->id === $id) return $friend->name;
+//   }
+//   return;
+// };
+
+// echo $findById('6') ?: 'No record found.';
+
+
+// $jsonitem = file_get_contents("data.json");
+
+//  $objitems = json_decode($jsonitem);
+
+
+//  $findById = function($id) use ($objname) {
+//   foreach (json_decode($objname) as $friend) {
+//     if ($friend->id === $id) return $friend->name;
+//   }
+//   return;
+// };
+
+// echo $findById('6') ?: 'No record found.';
+
+// $bien = (double)str_replace(",", "", "236,738,751,336.01");
+// $bien++;
+// echo $bien;
+// include($_SERVER['DOCUMENT_ROOT']."\Stock_market_PHP_1\Assets\others\simple_html_dom\simple_html_dom.php");
+// $html = str_get_html(file_get_html('https://finance.vietstock.vn/'));
+// echo $html;
+
+
